624c82757997052ca4fe1a003d310cb4fb9c31f2
Morven Lewis-Everley <morven@i-lateral.com>
2016-07-08 23:14:56 +0100

    Only add customisations if a product exists

--- code/model/CustomisableProduct.php
+++ code/model/CustomisableProduct.php
@@ -22,53 +22,56 @@ class CustomisableProduct extends Product
 
         $fields->removeByName("Root.Customisations");
 
-        $fields->addFieldToTab(
-            'Root',
-            new Tab(
-                'Customisations',
-                'Customisations'
-            ),
-            'Settings'
-        );
-
-        // Deal with customisations
-        $add_button = new GridFieldAddNewButton('toolbar-header-left');
-        $add_button->setButtonName(_t(
-            "CustomisableProduct.AddCustomisation",
-            "Add Customisation"
-        ));
-
-        $custom_config = GridFieldConfig::create()->addComponents(
-            new GridFieldToolbarHeader(),
-            $add_button,
-            new GridFieldSortableHeader(),
-            new GridFieldDataColumns(),
-            new GridFieldPaginator(20),
-            new GridFieldEditButton(),
-            new GridFieldDeleteAction(),
-            new GridFieldDetailForm(),
-            new GridFieldOrderableRows('Sort')
-        );
-
-        $fields->addFieldsToTab(
-            'Root.Customisations',
-            array(
-                DropdownField::create(
-                    "CustomisationListID",
-                    _t("CustomisableProduct.UseCustomisationList", "Use a Customisation List"),
-                    ProductCustomisationList::get()->map()
-                )->setEmptyString(_t(
-                    "CustomisableProduct.SelectList",
-                    "Select List"
-                )),
-                GridField::create(
+        // Only add fields if the object exists
+        if($this->ID) {
+            $fields->addFieldToTab(
+                'Root',
+                new Tab(
                     'Customisations',
-                    '',
-                    $this->Customisations(),
-                    $custom_config
+                    'Customisations'
+                ),
+                'Settings'
+            );
+
+            // Deal with customisations
+            $add_button = new GridFieldAddNewButton('toolbar-header-left');
+            $add_button->setButtonName(_t(
+                "CustomisableProduct.AddCustomisation",
+                "Add Customisation"
+            ));
+
+            $custom_config = GridFieldConfig::create()->addComponents(
+                new GridFieldToolbarHeader(),
+                $add_button,
+                new GridFieldSortableHeader(),
+                new GridFieldDataColumns(),
+                new GridFieldPaginator(20),
+                new GridFieldEditButton(),
+                new GridFieldDeleteAction(),
+                new GridFieldDetailForm(),
+                new GridFieldOrderableRows('Sort')
+            );
+
+            $fields->addFieldsToTab(
+                'Root.Customisations',
+                array(
+                    DropdownField::create(
+                        "CustomisationListID",
+                        _t("CustomisableProduct.UseCustomisationList", "Use a Customisation List"),
+                        ProductCustomisationList::get()->map()
+                    )->setEmptyString(_t(
+                        "CustomisableProduct.SelectList",
+                        "Select List"
+                    )),
+                    GridField::create(
+                        'Customisations',
+                        '',
+                        $this->Customisations(),
+                        $custom_config
+                    )
                 )
-            )
-        );
+            );
+        }
 
         return $fields;
     }
