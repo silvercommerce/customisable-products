1a1b49da8b009330f11ec776c7a963a8d78b0804
Morven Lewis-Everley <morven@i-lateral.com>
2024-11-18 13:26:11 +0000

    Add custom helper to handle adding items to the cart

---
+++ src/CartCustomisationHelper.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace SilverCommerce\CustomisableProducts;
+
+use LogicException;
+use SilverStripe\ORM\ArrayList;
+use SilverCommerce\OrdersAdmin\Factory\LineItemFactory;
+use SilverCommerce\OrdersAdmin\Interfaces\LineItemPricable;
+use SilverCommerce\OrdersAdmin\Interfaces\LineItemCustomisable;
+use SilverStripe\ORM\SS_List;
+
+class CartCustomisationHelper implements LineItemPricable, LineItemCustomisable
+{
+    protected function extractCustomisationID(string $key): int
+    {
+        $data = explode("_", $key);
+        return $data[1];
+    }
+
+    protected function isDataCustomisation(string $key): bool
+    {
+        return !(strpos($key, 'customise') === false);
+    }
+
+    protected function getProductCustomisation(string $key): ProductCustomisation
+    {
+        $id = $this->extractCustomisationID($key);
+        $item = ProductCustomisation::get()
+            ->byID($id);
+
+        if (empty($item)) {
+            throw new LogicException('Invalid customisation');
+        }
+
+        return $item;
+    }
+
+    public function modifyItemPrice(
+        LineItemFactory $factory,
+        array $data = []
+    ): void {
+        $item = $factory->getItem();
+
+        if (empty($item)) {
+            return;
+        }
+
+        $product = $item->findStockItem();
+
+        if (!$product instanceof CustomisableProduct) {
+            return;
+        }
+
+        foreach ($data as $key => $value) {
+            if (!$this->isDataCustomisation($key)) {
+                continue;
+            }
+
+            $custom_item = $this->getProductCustomisation($key);
+            $modify = 0;
+
+            $option = $custom_item
+                ->Options()
+                ->find("Title", $value);
+
+            if (!empty($option)) {
+                $modify = $option->ModifyPrice;
+            }
+
+            if (!(round($modify, 2) === 0.00)) {
+                $factory->modifyPrice(
+                    $custom_item->Title,
+                    (float)$option->ModifyPrice,
+                    $product
+                );
+            }
+        }
+
+        return;
+    }
+
+    public function customiseLineItem(
+        LineItemFactory $factory,
+        array $data = []
+    ): void {
+        $item = $factory->getItem();
+        $product = $item->findStockItem();
+
+        if (!$product instanceof CustomisableProduct) {
+            return;
+        }
+
+        foreach ($data as $key => $value) {
+            if (!$this->isDataCustomisation($key)) {
+                continue;
+            }
+
+            $custom_item = $this->getProductCustomisation($key);
+            $option = null;
+
+            if ($custom_item->DisplayAs === ProductCustomisation::TEXT_FIELD) {
+                $custom_value = $value;
+            } elseif (is_array($value)) {
+                $custom_value = implode(",", $value);
+            } else {
+                $option = $custom_item
+                    ->Options()
+                    ->find("Title", $value);
+
+                if (empty($option)) {
+                    throw new LogicException('Invalid customisation option');
+                }
+
+                $custom_value = $option->Title;
+            }
+
+            $factory->customise(
+                $custom_item->Title,
+                $custom_value,
+                [],
+                $option
+            );
+        }
+
+        return;
+    }
+}
--- src/CustomisableProductController.php
+++ src/CustomisableProductController.php
@@ -2,9 +2,7 @@
 
 namespace SilverCommerce\CustomisableProducts;
 
-use Exception;
 use ProductController;
-use SilverStripe\ORM\ValidationResult;
 use SilverCommerce\ShoppingCart\Forms\AddToCartForm;
 use SilverCommerce\ShoppingCart\ShoppingCartFactory;
 
@@ -72,88 +70,5 @@ class CustomisableProductController extends ProductController
         }
 
         return $form;
-    }
-
-    public function doAddItemToCart($data, $form)
-    {
-        $classname = $data["ClassName"];
-        $id = $data["ID"];
-        $object = $classname::get()->byID($id);
-        $cart = ShoppingCartFactory::create();
-        $customisations = [];
-
-        if (!empty($object)) {
-            foreach ($data as $key => $value) {
-                if (!(strpos($key, 'customise') === false) && $value) {
-                    $custom_data = explode("_", $key);
-
-                    if ($custom_item = ProductCustomisation::get()->byID($custom_data[1])) {
-                        $modify_price = 0;
-
-                        // Deal with checkbox set fields to ensure data is a string
-                        if (is_array($value)) {
-                            $custom_value = implode(",", $value);
-                        } else {
-                            $custom_value = $value;
-                        }
-
-                        // Check if the current option has a price modification
-                        if ($custom_item->Options()->exists()) {
-                            $options = $custom_item
-                                ->Options()
-                                ->filter("Title", $value);
-
-                            // If dealing with multiple results collect them
-                            // or return a single value
-                            if ($options->exists() && $options->count() > 1) {
-                                $custom_value = "";
-                                foreach ($options as $option) {
-                                    $modify_price = $modify_price + $option->ModifyPrice;
-                                    $custom_value .= $option->Title;
-                                }
-                            } elseif ($options->exists()) {
-                                $option = $options->first();
-                                $modify_price = $option->ModifyPrice;
-                            }
-                        }
-
-                        $customisations[] = [
-                            "Title" => $custom_item->Title,
-                            "Value" => $custom_value,
-                            "BasePrice" => $modify_price
-                        ];
-                    }
-                }
-            }
-            
-
-            // Try and add item to cart, return any exceptions raised
-            // as a message
-            try {
-                $cart->addItem($object, $data['Quantity'], false, $customisations);
-                $cart->save();
-
-                $message = _t(
-                    'ShoppingCart.AddedItemToCart',
-                    'Added "{item}" to your shopping cart',
-                    ["item" => $object->Title]
-                );
-
-                $form->sessionMessage(
-                    $message,
-                    ValidationResult::TYPE_GOOD
-                );
-            } catch(Exception $e) {
-                $form->sessionMessage(
-                    $e->getMessage()
-                );
-            }
-        } else {
-            $form->sessionMessage(
-                _t("ShoppingCart.ErrorAddingToCart", "Error adding item to cart")
-            );
-        }
-
-        return $this->redirectBack();
     }
 }
