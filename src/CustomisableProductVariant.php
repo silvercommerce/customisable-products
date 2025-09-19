0bad6b7ece349af0255d0162ee55e379832bbf0e
Morven Lewis-Everley <morven@i-lateral.com>
2025-09-19 16:02:48 +0000

    Add Pricable to variation and custom image

--- src/CustomisableProductVariant.php
+++ src/CustomisableProductVariant.php
@@ -2,8 +2,16 @@
 
 namespace SilverCommerce\CustomisableProducts;
 
+use SilverStripe\i18n\i18n;
+use SilverStripe\View\HTML;
+use SilverStripe\Assets\Image;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\ManyManyList;
+use SilverStripe\Versioned\Versioned;
+use SilverStripe\SiteConfig\SiteConfig;
+use SilverCommerce\TaxAdmin\Model\TaxRate;
+use SilverCommerce\TaxAdmin\Traits\Taxable;
+use SilverCommerce\TaxAdmin\Interfaces\TaxableProvider;
 use SilverCommerce\CustomisableProducts\ProductCustomisationOption;
 
 /**
@@ -18,16 +26,19 @@ use SilverCommerce\CustomisableProducts\ProductCustomisationOption;
  * @method ProductCustomisation Customisation
  * @method ManyManyList Options
  */
-class CustomisableProductVariant extends DataObject
+class CustomisableProductVariant extends DataObject implements TaxableProvider
 {
+    use Taxable;
+
     private static $table_name = "CustomisableProductVariant";
 
     private static $db = [
         'StockID' => 'Varchar',
-        'Baseprice' => 'Decimal(9,3)'
+        'BasePrice' => 'Decimal(9,3)'
     ];
 
     private static $has_one = [
+        'Image' => Image::class,
         'Parent' => CustomisableProduct::class
     ];
 
@@ -40,6 +51,74 @@ class CustomisableProductVariant extends DataObject
         'StockID',
         'BasePrice'
     ];
+
+    private static $casting = [
+        'OptionsList' => 'Varchar',
+        'CMSThumbnailHTML' => 'HTMLText'                            
+    ];
+
+    private static $extensions = [
+        Versioned::class . '.versioned'
+    ];
+
+    public function getTitle(): string
+    {
+        return (string)$this->getOptionsList();
+    }
+
+    public function getBasePrice()
+    {
+        return $this
+            ->dbObject('BasePrice')
+            ->getValue();
+    }
+
+    public function getShowPriceWithTax(): bool
+    {
+        return $this->Parent()->getShowPriceWithTax();
+    }
+
+    public function getShowTaxString(): bool
+    {
+        return $this->Parent()->getShowTaxString();
+    }
+
+    public function getTaxRate(): TaxRate
+    {
+        return $this->Parent()->getTaxRate();
+    }
+
+    public function getLocale(): string
+    {
+        return $this->Parent()->getLocale();
+    }
+
+    public function getOptionsList(): string
+    {
+        $options = $this
+            ->Options()
+            ->sort('Title ASC')
+            ->column('Title');
+
+        return implode(', ', $options);
+    }
+
+    /**
+     * Stub method to get the site config, unless the current class can provide an alternate.
+     *
+     * @return SiteConfig
+     */
+    public function getSiteConfig()
+    {
+        if ($this->hasMethod('alternateSiteConfig')) {
+            $altConfig = $this->alternateSiteConfig();
+            if ($altConfig) {
+                return $altConfig;
+            }
+        }
+
+        return SiteConfig::current_site_config();
+    }
 
     public function getCMSFields()
     {
