134d49e3731b6131d46d117cc19226d50612ff34
Morven Lewis-Everley <morven@i-lateral.com>
2025-09-19 16:16:48 +0000

    Update product controller to have a json based action and break out AddToCartForm

---
+++ src/CustomisableAddToCartForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SilverCommerce\CustomisableProducts;
+
+use SilverCommerce\ShoppingCart\Forms\AddToCartForm;
+use SilverCommerce\ShoppingCart\ShoppingCartFactory;
+
+class CustomisableAddToCartForm extends AddToCartForm
+{
+    public function __construct(
+        CustomisableProductController $controller,
+        $name = self::DEFAULT_NAME,
+    ) {
+        parent::__construct($controller, $name);
+
+        /** @var CustomisableProduct $object */
+        $product = $controller->dataRecord;
+        $link = $controller->getCustomisationLink();
+        $list = $product->getCompiledCustomisations();
+
+        $this->setHTMLID('AddToCartForm_AddToCartForm');
+        $this->setProductClass($product->ClassName);
+        $this->setProductID($product->ID);
+        $this->setAttribute(
+            'data-customisation-link',
+            $link
+        );
+        $this->addExtraClass("product-customisation-form");
+
+        foreach ($list as $customisation) {
+            $field = $customisation
+                ->Field()
+                ->addExtraClass("product-customisation-field");
+
+            $this
+                ->Fields()
+                ->insertBefore("Quantity", $field);
+
+            // Check if field required
+            if ($customisation->Required) {
+                // Manualy make field required (as SS can ignore this step)
+                $field
+                    ->setAttribute("required", true)
+                    ->addExtraClass("required");
+
+                $this
+                    ->getValidator()
+                    ->addRequiredField($field->getName());
+            }
+        }
+
+        $this->extend("updateCustomisableAddToCartForm");
+
+        return;
+    }
+}
--- src/CustomisableProductController.php
+++ src/CustomisableProductController.php
@@ -3,72 +3,90 @@
 namespace SilverCommerce\CustomisableProducts;
 
 use ProductController;
+use SilverStripe\Control\HTTPRequest;
 use SilverCommerce\ShoppingCart\Forms\AddToCartForm;
-use SilverCommerce\ShoppingCart\ShoppingCartFactory;
+use SilverCommerce\CustomisableProducts\CustomisableProduct;
+use SilverStripe\View\Requirements;
 
 class CustomisableProductController extends ProductController
 {
     private static $allowed_actions = [
+        "customisationdata",
         "AddToCartForm"
     ];
 
+    public function init()
+    {
+        parent::init();
+
+        Requirements::javascript(
+            'silvercommerce/customisable-products: client/dist/js/main.bundle.js'
+        );
+    }
+
+    public function getCustomisationLink()
+    {
+        return $this->Link('customisationdata');
+    }
+
+    public function customisationdata(HTTPRequest $request)
+    {
+        /** @var CustomisableProduct */
+        $record = $this->dataRecord;
+        $ids = $request->getVar('o');
+
+        if (empty($ids)) {
+            return $this->httpError(500);
+        }
+
+        $ids = explode(',', $ids);
+        $options = [];
+
+        if (count($ids) === 0) {
+            return $this->httpError(500);
+        }
+
+        $options = ProductCustomisationOption::get()
+            ->filter('ID', $ids)
+            ->toArray();
+
+        $variant = $record->findVariationByOptions($options);
+
+        if (empty($variant)) {
+            return $this->httpError(404);
+        }
+
+        $image_url = null;
+        $image_id = null;
+
+        if ($variant->Image()->exists()) {
+            $image = $variant->Image();
+            $image_url = $image->Pad(1200,1200)->getURL();
+            $image_id = $image->ID;
+        }
+
+        $data = [
+            'id' => $variant->ID,
+            'iid' => $image_id,
+            'isrc' => $image_url,
+            'price' => (string)$variant->getNicePrice(),
+            'stockid' => $variant->StockID,
+        ];
+
+        $response = $this
+            ->getResponse()
+            ->addHeader('Content-Type', 'application/json')
+            ->setStatusCode(200)
+            ->setBody(json_encode($data));
+
+        return $response;
+    }
+
     public function AddToCartForm()
     {
-        $form = AddToCartForm::create(
-            $this->owner,
-            "AddToCartForm"
+        return CustomisableAddToCartForm::create(
+            $this,
+            'AddToCartForm'
         );
-        $object = $this->dataRecord;
-        $list = $object->CustomisationList();
-
-        $form
-            ->setProductClass($object->ClassName)
-            ->setProductID($object->ID);
-
-        // First add customisations from global lists
-        if ($list->exists()) {
-            foreach ($list->Customisations() as $customisation) {
-                $field = $customisation->Field();
-                $form
-                    ->Fields()
-                    ->insertBefore($field, "Quantity");
-
-                // Check if field required
-                if ($customisation->Required) {
-                    // Manualy make field required (as SS seems to ignore this step)
-                    $field
-                        ->setAttribute("required", true)
-                        ->addExtraClass("required");
-
-                    $form
-                        ->getValidator()
-                        ->addRequiredField($field->getName());
-                }
-            }
-        }
-
-        // If product colour customisations are set, add them to the item form
-        if ($object->Customisations()->exists()) {
-            foreach ($object->Customisations() as $customisation) {
-                $field = $customisation->Field();
-                $form
-                    ->Fields()
-                    ->insertBefore($field, "Quantity");
-
-                // Check if field required
-                if ($customisation->Required) {
-                    // Manualy make field required (as SS seems to ignore this step)
-                    $field
-                        ->setAttribute("required", true)
-                        ->addExtraClass("required");
-
-                    $form
-                        ->getValidator()
-                        ->addRequiredField($field->getName());
-                }
-            }
-        }
-
-        return $form;
     }
 }
