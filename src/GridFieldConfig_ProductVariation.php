7deed59b7b264cd19ecc7db37ceffb0756462c09
Morven Lewis-Everley <morven@i-lateral.com>
2025-09-19 16:13:09 +0000

    Add custom gridfield config

---
+++ src/GridFieldConfig_ProductVariation.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace SilverCommerce\CustomisableProducts;
+
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use SilverStripe\Forms\GridField\GridFieldSortableHeader;
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Forms\TextField;
+use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
+
+class GridFieldConfig_ProductVariation extends GridFieldConfig
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $columns = new GridFieldEditableColumns();
+        $columns->setDisplayFields([
+            'Title' => [
+                'title' => _t(
+                    "CustomisableProduct.Title",
+                    "Title"
+                ),
+                'field' => ReadonlyField::class
+            ],
+            'ImageID' => [
+                'title' => _t(
+                    "CustomisableProduct.Image",
+                    "Image"
+                ),
+                'callback' => function ($record, $column, $grid) {
+                    return DropdownField::create(
+                        $column,
+                        null,
+                        $record
+                            ->Parent()
+                            ->Images()
+                            ->map('ID', 'Title')
+                            ->toArray(),
+                    );
+                },
+            ],
+            'StockID' => [
+                'title' => _t(
+                    "CustomisableProduct.StockID",
+                    "StockID"
+                ),
+                'field' => TextField::class
+            ],
+            'BasePrice' => [
+                'title' => _t(
+                    "CustomisableProduct.Price",
+                    "Price"
+                ),
+                'callback' => function ($record, $column, $grid) {
+                    return $record
+                        ->dbObject('BasePrice')
+                        ->scaffoldFormField();
+                }
+            ]
+        ]);
+
+        $this->addComponent(new GridFieldToolbarHeader());
+        $this->addComponent(new GridFieldSortableHeader());
+        $this->addComponent($columns);
+        $this->addComponent(new GridFieldDetailForm());
+    }
+}
